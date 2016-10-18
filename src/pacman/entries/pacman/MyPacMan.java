1dd7ea590570e67eccf4f33c87954e90abf36d6d
cegimo <cesar@MacBook-Pro-de-Cesar.local>
2016-10-18 20:53:09 +0200

    Created controller
    Faltan los comentarios

--- src/pacman/entries/pacman/MyPacMan.java
+++ src/pacman/entries/pacman/MyPacMan.java
@@ -28,11 +28,69 @@ private DecisionTree decisionTree;
 	}
 	
 	
+	
 	public MOVE getMove(Game game,long timeDue)
 	{			
 		String strategy = decisionTree.getStrategy(game);
+		int current=game.getPacmanCurrentNodeIndex();
 		
 		System.out.println(strategy);
-		return MOVE.DOWN;
+		
+		if(strategy == "huir"){
+			//Strategy 1: if any non-edible ghost is too close (less than MIN_DISTANCE), run away
+			for(GHOST ghost : GHOST.values())
+				if(game.getGhostEdibleTime(ghost)==0 && game.getGhostLairTime(ghost)==0)
+					if(game.getShortestPathDistance(current,game.getGhostCurrentNodeIndex(ghost))<MIN_DISTANCE){			
+						return game.getNextMoveAwayFromTarget(game.getPacmanCurrentNodeIndex(),game.getGhostCurrentNodeIndex(ghost),DM.PATH);
+					}
+		}
+		
+		if(strategy == "cazar"){
+			//Strategy 2: find the nearest edible ghost and go after them 
+			int minDistance=Integer.MAX_VALUE;
+			GHOST minGhost=null;		
+			
+			for(GHOST ghost : GHOST.values())
+				if(game.getGhostEdibleTime(ghost)>0)
+				{
+					int distance=game.getShortestPathDistance(current,game.getGhostCurrentNodeIndex(ghost));
+					
+					if(distance<minDistance)
+					{
+						minDistance=distance;
+						minGhost=ghost;
+					}
+				}
+			
+			if(minGhost!=null){	//we found an edible ghost
+				
+				return game.getNextMoveTowardsTarget(game.getPacmanCurrentNodeIndex(),game.getGhostCurrentNodeIndex(minGhost),DM.PATH);
+			}
+		}
+		if(strategy == "comer"){
+			//Strategy 3: go after the pills and power pills
+			int[] pills=game.getPillIndices();
+			int[] powerPills=game.getPowerPillIndices();		
+			
+			ArrayList<Integer> targets=new ArrayList<Integer>();
+			
+			for(int i=0;i<pills.length;i++)					//check which pills are available			
+				if(game.isPillStillAvailable(i))
+					targets.add(pills[i]);
+			
+			for(int i=0;i<powerPills.length;i++)			//check with power pills are available
+				if(game.isPowerPillStillAvailable(i))
+					targets.add(powerPills[i]);				
+			
+			int[] targetsArray=new int[targets.size()];		//convert from ArrayList to array
+			
+			for(int i=0;i<targetsArray.length;i++)
+				targetsArray[i]=targets.get(i);
+			//Set de la estrategia
+			game.setStrategy("comer");
+			//return the next direction once the closest target has been identified
+			return game.getNextMoveTowardsTarget(current,game.getClosestNodeIndexFromNodeIndex(current,targetsArray,DM.PATH),DM.PATH);
+		}
+		return MOVE.NEUTRAL;
 	}
 }
